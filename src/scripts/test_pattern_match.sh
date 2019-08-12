dd3d655ee1634847fce869ab027338af1d6690ed
matentzn <nicolas.matentzoglu@gmail.com>
2019-08-12 15:05:58 +0100

    win pipeline

--- src/scripts/test_pattern_match.sh
+++ src/scripts/test_pattern_match.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sh run.sh dosdp-tools query --ontology=../curation/ontologies-for-matching/mp.owl --reasoner=elk --obo-prefixes=true --template=../curation/patterns-for-matching/abnormalAnatomicalEntity.yaml --outfile=../curation/pattern-matches/mp/abnormalAnatomicalEntity.tsv
+dosdp-tools query --ontology=../curation/ontologies-for-matching/mp.owl --reasoner=elk --obo-prefixes=true --template=../curation/patterns-for-matching/abnormalAnatomicalEntity.yaml --outfile=../curation/pattern-matches/mp/abnormalAnatomicalEntity.tsv
