e9184ee5f65758a29dcc45abdd4f0ef257466732
matentzn <nicolas.matentzoglu@gmail.com>
2019-11-14 07:49:24 +0000

    Update upheno_pipeline_jenkins.sh

--- src/scripts/upheno_pipeline_jenkins.sh
+++ src/scripts/upheno_pipeline_jenkins.sh
@@ -5,6 +5,9 @@ set -e
 # Author: Nicolas Matentzoglu, European Bioinformatics Institute (EMBL-EBI)
 # Monarch Initiative, https://monarchinitiative.org
 
+ls -l ../sparql
+ls -l ../ontology
+pwd
 python3 upheno_prepare.py ../curation/upheno-config.yaml
 python3 upheno_create_profiles.py ../curation/upheno-config.yaml
 
