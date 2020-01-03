d7af9304b0a8f2f487ba72797844eff6fb349c2e
matentzn <nicolas.matentzoglu@gmail.com>
2020-01-03 14:16:25 +0000

    Old files

--- src/scripts/test_pattern_match.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dosdp-tools query --ontology=../curation/ontologies-for-matching/mp.owl --reasoner=elk --obo-prefixes=true --template=../curation/patterns-for-matching/abnormalAnatomicalEntity.yaml --outfile=../curation/pattern-matches/mp/abnormalAnatomicalEntity.tsv
