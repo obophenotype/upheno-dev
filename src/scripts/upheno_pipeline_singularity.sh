1719acbd21a469f2eedd89c23fdc5cf50a948140
matentzn <nicolas.matentzoglu@gmail.com>
2020-01-08 13:26:23 +0000

    Update upheno_pipeline_singularity.sh

--- src/scripts/upheno_pipeline_singularity.sh
+++ src/scripts/upheno_pipeline_singularity.sh
@@ -5,5 +5,5 @@ data=/nfs/production3/spot/sw/dev/monarch/data/upheno-dev/src/scripts
 
 cd $data
 #singularity pull docker://obolibrary/odkfull
-singularity shell --pwd $data docker://obolibrary/odkfull -c "python3 upheno_prepare.py ../curation/upheno-config.yaml"
-singularity shell --pwd $data docker://obolibrary/odkfull -c "python3 upheno_create_profiles.py ../curation/upheno-config.yaml"
+singularity exec --pwd $data docker://obolibrary/odkfull python3 upheno_prepare.py ../curation/upheno-config.yaml
+singularity exec --pwd $data docker://obolibrary/odkfull python3 upheno_create_profiles.py ../curation/upheno-config.yaml
