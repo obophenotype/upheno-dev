4976556a5172f381f0d01051c43225b5724bf9a5
matentzn <nicolas.matentzoglu@gmail.com>
2020-01-29 11:52:14 +0000

    Update upheno_pipeline_jenkins.sh

--- src/scripts/upheno_pipeline_jenkins.sh
+++ src/scripts/upheno_pipeline_jenkins.sh
@@ -9,8 +9,9 @@ pwd
 ls -l ../sparql
 ls -l ../ontology
 ls -l ../curation
-echo "REMOVING XPO.OWL REMOVEREMOVEREMOVE"
-rm ../curation/ontologies-for-matching/xpo.owl
+echo "REMOVING ALLIMPORTSMERGED.OWL REMOVEREMOVEREMOVE"
+rm -f ../curation/tmp/upheno-allimports-merged.owl
+rm -f ../curation/tmp/upheno-allimports-dosdp.owl
 python3 upheno_prepare.py ../curation/upheno-config.yaml
 python3 upheno_create_profiles.py ../curation/upheno-config.yaml
 python3 upheno-stats.py ../curation/upheno-config.yaml
