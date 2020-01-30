9c7f45101349a1363ffea06011483f47b4d05d20
matentzn <nicolas.matentzoglu@gmail.com>
2020-01-30 00:25:44 +0000

    commented out pipeline

--- src/scripts/upheno_pipeline_jenkins.sh
+++ src/scripts/upheno_pipeline_jenkins.sh
@@ -9,10 +9,11 @@ pwd
 ls -l ../sparql
 ls -l ../ontology
 ls -l ../curation
-echo "REMOVING ALLIMPORTSMERGED.OWL REMOVEREMOVEREMOVE"
-rm -f ../curation/tmp/upheno-allimports-merged.owl
-rm -f ../curation/tmp/upheno-allimports-dosdp.owl
-python3 upheno_prepare.py ../curation/upheno-config.yaml
-python3 upheno_create_profiles.py ../curation/upheno-config.yaml
-python3 upheno-stats.py ../curation/upheno-config.yaml
+#echo "REMOVING ALLIMPORTSMERGED.OWL REMOVEREMOVEREMOVE"
+#sh explain.sh "" 
+#rm -f ../curation/tmp/upheno-allimports-merged.owl
+#rm -f ../curation/tmp/upheno-allimports-dosdp.owl
+#python3 upheno_prepare.py ../curation/upheno-config.yaml
+#python3 upheno_create_profiles.py ../curation/upheno-config.yaml
+#python3 upheno-stats.py ../curation/upheno-config.yaml
 
