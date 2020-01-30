62516dd25950515f5283f6c577f6f43b0318cf1f
matentzn <nicolas.matentzoglu@gmail.com>
2020-01-30 00:32:29 +0000

    Update upheno_pipeline_jenkins.sh

--- src/scripts/upheno_pipeline_jenkins.sh
+++ src/scripts/upheno_pipeline_jenkins.sh
@@ -9,9 +9,11 @@ pwd
 ls -l ../sparql
 ls -l ../ontology
 ls -l ../curation
-#echo "REMOVING ZP.OWL REMOVEREMOVEREMOVE"
-#rm ../curation/ontologies-for-matching/zp.owl
-python3 upheno_prepare.py ../curation/upheno-config.yaml
-python3 upheno_create_profiles.py ../curation/upheno-config.yaml
-python3 upheno-stats.py ../curation/upheno-config.yaml
+#echo "REMOVING ALLIMPORTSMERGED.OWL REMOVEREMOVEREMOVE"
+#sh explain.sh "" 
+#rm -f ../curation/tmp/upheno-allimports-merged.owl
+#rm -f ../curation/tmp/upheno-allimports-dosdp.owl
+#python3 upheno_prepare.py ../curation/upheno-config.yaml
+#python3 upheno_create_profiles.py ../curation/upheno-config.yaml
+#python3 upheno-stats.py ../curation/upheno-config.yaml
 
