d8b97a9ee7b66d65a9e2a3080bdf91c571d11740
matentzn <nicolas.matentzoglu@gmail.com>
2020-01-30 22:22:51 +0000

    Update upheno_pipeline_jenkins.sh

--- src/scripts/upheno_pipeline_jenkins.sh
+++ src/scripts/upheno_pipeline_jenkins.sh
@@ -6,15 +6,14 @@ set -e
 # Monarch Initiative, https://monarchinitiative.org
 
 pwd
-ls -l ../sparql
 ls -l ../ontology
 ls -l ../curation
 #echo "REMOVING ALLIMPORTSMERGED.OWL REMOVEREMOVEREMOVE"
-sh explain.sh "nurse cell" ../curation/tmp/upheno-allimports-merged.owl ../curation/tmp/ex_nursecell.owl
-sh explain.sh "adult intestinal epithelium" ../curation/tmp/upheno-allimports-merged.owl ../curation/tmp/ex_adultintestinal.owl
-#rm -f ../curation/tmp/upheno-allimports-merged.owl
-#rm -f ../curation/tmp/upheno-allimports-dosdp.owl
-#python3 upheno_prepare.py ../curation/upheno-config.yaml
-#python3 upheno_create_profiles.py ../curation/upheno-config.yaml
-#python3 upheno-stats.py ../curation/upheno-config.yaml
+#sh explain.sh "nurse cell" ../curation/tmp/upheno-allimports-merged.owl ../curation/tmp/ex_nursecell.owl
+#sh explain.sh "adult intestinal epithelium" ../curation/tmp/upheno-allimports-merged.owl ../curation/tmp/ex_adultintestinal.owl
+rm -f ../curation/tmp/upheno-allimports-merged.owl
+rm -f ../curation/tmp/upheno-allimports-dosdp.owl
+python3 upheno_prepare.py ../curation/upheno-config.yaml
+python3 upheno_create_profiles.py ../curation/upheno-config.yaml
+python3 upheno-stats.py ../curation/upheno-config.yaml
 
