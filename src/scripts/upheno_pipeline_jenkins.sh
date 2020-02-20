58f515f38b6c3b65ad457fea349e6a704f9b1f64
matentzn <nicolas.matentzoglu@gmail.com>
2020-02-20 14:32:20 +0000

    Get ready for new run

--- src/scripts/upheno_pipeline_jenkins.sh
+++ src/scripts/upheno_pipeline_jenkins.sh
@@ -9,8 +9,7 @@ pwd
 ls -l ../sparql
 ls -l ../ontology
 ls -l ../curation
-echo "REMOVING XPO.OWL REMOVEREMOVEREMOVE"
-rm ../curation/ontologies-for-matching/xpo.owl
+
 python3 upheno_prepare.py ../curation/upheno-config.yaml
 python3 upheno_create_profiles.py ../curation/upheno-config.yaml
 python3 upheno-stats.py ../curation/upheno-config.yaml
