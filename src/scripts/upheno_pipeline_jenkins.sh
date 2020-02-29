0a911e0e82557ffab342e1fc735bbac981a12f87
matentzn <nicolas.matentzoglu@gmail.com>
2020-02-29 12:18:52 +0000

    Update upheno_pipeline_jenkins.sh

--- src/scripts/upheno_pipeline_jenkins.sh
+++ src/scripts/upheno_pipeline_jenkins.sh
@@ -15,10 +15,20 @@ ls -l ../curation
 #rm -f ../curation/tmp/upheno-allimports-merged.owl
 #rm -f ../curation/tmp/upheno-allimports-dosdp.owl
 
-python3 upheno_prepare.py ../curation/upheno-config.yaml
-
+#python3 upheno_prepare.py ../curation/upheno-config.yaml
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
 python3 upheno_create_profiles.py ../curation/upheno-config.yaml
 
 python3 upheno-stats.py ../curation/upheno-config.yaml
-
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
+echo "PREPARESKIPPED"
 
