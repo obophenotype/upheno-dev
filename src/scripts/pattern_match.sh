bf8f7520095a2c56260aeea5849abedf95e9dc46
Nico Matentzoglu <nicolas.matentzoglu@gmail.com>
2021-10-22 15:04:42 +0300

    Update patterns pipeline

--- src/scripts/pattern_match.sh
+++ src/scripts/pattern_match.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 ONTDIR=../scripts/pattern-matches-oneoff/ontologies/
-TEMPLATEDIR=../scripts/pattern-matches-oneoff/upheno_patterns/
+TEMPLATEDIR=../scripts/pattern-matches-oneoff/patterns/
 TSVDIR=../scripts/pattern-matches-oneoff/matches/
 
 ONTS="mp hp xpo wbphenotype zp"
@@ -29,6 +29,6 @@ for o in ${ONTDIR}*.owl
 do
 	ONT=${o}
 	TSVONT=${TSVDIR}$(basename "$o" .owl)
-	mkdir $TSVONT
+	mkdir -p $TSVONT
 	sh run.sh dosdp-tools query --ontology=$ONT --reasoner=elk --obo-prefixes=true --template=$TEMPLATEDIR --batch-patterns="${PATTERNS}" --outfile=$TSVONT
 done
